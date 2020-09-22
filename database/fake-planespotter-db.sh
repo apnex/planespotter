80c604bb025fbffb5f0b330a67c1eedfa52241fb
root <aobersnel@apnex.com.au>
2020-09-22 16:49:59 +1000

    Updates...

--- database/fake-planespotter-db.sh
+++ database/fake-planespotter-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/planespotter/db-install/
-curl -Lo MASTER.txt https://raw.githubusercontent.com/apnex/labops/master/database/FAKE-MASTER.txt
-curl -Lo ACFTREF.txt https://raw.githubusercontent.com/apnex/labops/master/database/FAKE-ACFTREF.txt
+curl -Lo MASTER.txt https://raw.githubusercontent.com/apnex/planespotter/master/database/FAKE-MASTER.txt
+curl -Lo ACFTREF.txt https://raw.githubusercontent.com/apnex/planespotter/master/database/FAKE-ACFTREF.txt
 mysql --local_infile=1 --user=root --password=${MYSQL_ROOT_PASSWORD} < create-planespotter-db.sql
 rm MASTER.txt ACFTREF.txt
